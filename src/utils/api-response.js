9ab15f9084bb81773fafc1a16f9fb81875bc80ad
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-04-15 20:57:39 +0530

    typos error in import export

--- src/utils/api-response.js
+++ src/utils/api-response.js
@@ -1,14 +1,10 @@
 class ApiResponse {
-    constructor(
-        statusCode,
-        data,
-        message = "Success"
-    ) {
+    constructor(statusCode, data, message = "Success") {
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
-        this.success = true;
+        this.success = statusCode < 400;
     }
 }
 
-export default ApiResponse
+export { ApiResponse };
