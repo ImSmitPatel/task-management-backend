66ca497480ff2e48198ec8d9887515b1e7d6746b
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-04-16 20:10:27 +0530

    Updated HealthCheck using ApiResponse util

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,10 +1,7 @@
 import {ApiResponse} from "../utils/api-response.js";
 
 const healthCheck = (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "Server is running",
-    })
+    res.status(200).json(new ApiResponse(200, {message: "Server is running"}))
 }
 
 export {healthCheck}
