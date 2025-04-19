ac498d11a0e29e3a4c18e196a05d5e981cdbcca4
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-04-19 19:09:42 +0530

    express json parser,  auth Router

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import express from "express";
 
 const app = express()
 
+app.use(express.json())
+
 // router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
 
 
 export default app;
